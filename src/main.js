c0f4829c3c48ec0971587753a5b8763f24372ff4
Laura <65158950+lauratro@users.noreply.github.com>
2021-05-07 14:27:19 +0200

    Created components Homepage

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./main.css";
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+
+function Main() {
+  return (
+    <>
+      <Header />
+      <HomeGuest />
+    </>
+  );
+}
+export default Main;
