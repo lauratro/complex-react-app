8a453eda1c4097088bdf0a272023fd67c648d34a
Laura <65158950+lauratro@users.noreply.github.com>
2021-05-07 14:54:14 +0200

    Created several pages

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./main.css";
 import Header from "./components/Header";
 import HomeGuest from "./components/HomeGuest";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
-    </>
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us" exact>
+          <About />
+        </Route>
+        <Route path="/terms" exact>
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 export default Main;
